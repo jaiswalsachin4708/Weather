2826c4df7303bc2373ace989a0c3825050694939
sachin jaiswal <sachinjaiswal4708@gmail.com>
2023-05-06 23:33:11 +0530

    update pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Tempapp from "./components/Tempapp"
 import './App.css';
+import Particle from "./particles";
 
 function App() {
   return (
-    <div >
+    <>
       <Tempapp/>
-    </div>
+      <Particle/>
+    </>
   );
 }
 
